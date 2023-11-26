d1ebcd345e35b8a8ea7b5339199549d328ca99f5
zev <totruongchinh@gmail.com>
2023-11-26 13:07:10 +0700

    select, update, delete, where - done- day 1

---
+++ src/Models/Customer.java
@@ -0,0 +1,11 @@
+package Models;
+
+import java.util.Date;
+
+public class Cutomer {
+    private int id;
+    private String name;
+    private String address;
+    private Date dayOfBirth;
+
+}
---
+++ src/Utils/JDBCConnect.java
@@ -0,0 +1,33 @@
+import java.sql.*;
+
+public class JDBCConnect {
+    public static Connection connect(){
+        Connection connection = null;
+        try{
+            // info
+            String url = "jdbc:mysql://localhost:3306/classicmodels ";
+            String usename = "root";
+            String password = "root";
+            // regist driver
+            connection = DriverManager.getConnection(url, usename, password);
+
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+        return connection;
+    }
+
+    public static void close(Connection c){
+        try{
+            c.close();
+        }
+        catch (SQLException e)
+        {
+            System.out.println(e);
+        }
+
+    }
+
+
+}
