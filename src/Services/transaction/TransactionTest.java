f04be290ac69ac015a79e1d8f8481cbc9891afe1
zev <totruongchinh@gmail.com>
2023-12-01 14:24:49 +0700

    finish all

--- src/Services/transaction/TransactionTest.java
+++ src/Services/transaction/TransactionTest.java
@@ -12,8 +12,8 @@ public class TransactionTest {
             Connection connection = JDBCConnect.connect();
             connection.setAutoCommit(false);
             Statement statement = connection.createStatement();
-            String insert1 = "insert into Employees values (30,'Manh',15.6)";
-            String insert2 = "insert into Employees values (0,'Manh',15.6)";
+            String insert1 = "insert into Employees values (20,'aaa',1.6)";
+            String insert2 = "insert into Employees values (0,'bbbb',1.6)";
             try{
                 statement.executeUpdate(insert1);
                 statement.executeUpdate(insert2);
